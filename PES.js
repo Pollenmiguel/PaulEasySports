22aebd07bcbb808ee56e1dc4d24054e5d645194b
Pollenmiguel <pollenmiguel@gmail.com>
2024-01-31 02:59:50 +0100

    optimisation YT

--- PES.js
+++ PES.js
@@ -1,6 +1,5 @@
 const header = document.querySelector("header");
-const logo = document.querySelector(".logo");
-
+const logo = header.querySelector(".logo"); // Sélection spécifique du logo à l'intérieur de l'élément header
 
 function updateHeader() {
     if (window.scrollY > 0) { // Changer ici pour inverser l'effet
